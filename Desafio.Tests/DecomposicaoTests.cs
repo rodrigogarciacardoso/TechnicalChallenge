9cfa122106d53a3879470f2f1c1c459703dbddb9
Rodrigo Cardoso <rodrigo_cardoso97@hotmail.com>
2021-12-04 09:34:39 -0300

    Projeto de teste e testes de decomposição e validação de primos

---
+++ Desafio.Tests/DecomposicaoTests.cs
@@ -0,0 +1,33 @@
+﻿using Desafio.Classes;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Desafio.Tests
+{
+    public class DecomposicaoTests
+    {
+        [Fact]
+        public void Decomposicao_RetornaDivisores_RetornarDivisoresDeUmNumero()
+        {
+            // Arrange
+            var decomposicao = new Decomposicao();
+
+            // Act
+            var resultado = decomposicao.RetornaDivisores(45);
+
+            // Assert
+            Assert.Equal(new HashSet<int> { 1, 3, 5, 9, 15, 45 }, resultado);
+        }
+
+        [Fact]
+        public void Decomposicao_RetornaDivisore_DeveRetornarArgumentException()
+        {
+            // Arrange
+            var decomposicao = new Decomposicao();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => decomposicao.RetornaDivisores(0));
+        }
+    }
+}
---
+++ Desafio.Tests/ValidarPrimosTests.cs
@@ -0,0 +1,33 @@
+﻿using Desafio.Classes;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Desafio.Tests
+{
+    public class ValidarPrimosTests
+    {
+        [Fact]
+        public void ValidarPrimos_RetornarPrimos_ValidarPrimosLista()
+        {
+            // Arrange
+            var validarPrimos = new ValidarPrimos();
+
+            // Act
+            var resultado = validarPrimos.RetornarPrimos(new HashSet<int> { 1, 3, 5, 9, 15, 45 });
+
+            // Assert
+            Assert.Equal(new HashSet<int> { 1, 3, 5 }, resultado);
+        }
+
+        [Fact]
+        public void ValidarPrimos_RetornarPrimos_DeveRetornarArgumentException()
+        {
+            // Arrange
+            var validarPrimos = new ValidarPrimos();
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => validarPrimos.RetornarPrimos(new HashSet<int> { }));
+        }
+    }
+}
