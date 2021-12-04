60fcae91819ea755ea5385b6b24252d467cebf76
Rodrigo Cardoso <rodrigo_cardoso97@hotmail.com>
2021-12-04 10:02:21 -0300

    Testes nas operações

---
+++ Desafio.Tests/OperacoesTests.cs
@@ -0,0 +1,64 @@
+﻿using Desafio.Classes;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Desafio.Tests
+{
+    public class OperacoesTests
+    {
+        [Fact]
+        public void Operacoes_RetornarDivisores_RetornaDivisoresNumero()
+        {
+            // Arrange
+            var decomposicao = new Decomposicao();
+            var validarPrimos = new ValidarPrimos();
+            var operacoes = new Operacoes.Operacoes(decomposicao, validarPrimos);
+
+            // Act
+            var resultado = operacoes.RetornarDivisores(45);
+
+            // Assert
+            Assert.Equal(new HashSet<int> { 1, 3, 5, 9, 15, 45 }, resultado);
+        }
+
+        [Fact]
+        public void Operacoes_RetornarDivisores_DeveRetornarArgumentException()
+        {
+            // Arrange
+            var decomposicao = new Decomposicao();
+            var validarPrimos = new ValidarPrimos();
+            var operacoes = new Operacoes.Operacoes(decomposicao, validarPrimos);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => operacoes.RetornarDivisores(0));
+        }
+
+        [Fact]
+        public void Operacoes_RetornarPrimos_RetornaPrimosDeLista()
+        {
+            // Arrange
+            var decomposicao = new Decomposicao();
+            var validarPrimos = new ValidarPrimos();
+            var operacoes = new Operacoes.Operacoes(decomposicao, validarPrimos);
+
+            // Act
+            var resultado = operacoes.RetornarPrimos(new HashSet<int> { 1, 3, 5, 9, 15, 45 });
+
+            // Assert
+            Assert.Equal(new HashSet<int> { 1, 3, 5 }, resultado);
+        }
+
+        [Fact]
+        public void Operacoes_RetornarPrimos_DeveRetornarArgumentException()
+        {
+            // Arrange
+            var decomposicao = new Decomposicao();
+            var validarPrimos = new ValidarPrimos();
+            var operacoes = new Operacoes.Operacoes(decomposicao, validarPrimos);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => operacoes.RetornarPrimos(new HashSet<int> { }));
+        }
+    }
+}
